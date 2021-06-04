b2734f15b3f60970977daaa322cc885f6a743078
Elijah Hensel <elijah.witth@gmail.com>
2021-06-04 12:55:39 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Pages from "./components/Pages";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <Navigation />
+      </header>
+      <main style={{ marginTop: "100px", display: "flex" }}>
+        <Pages />
+      </main>
+    </div>
+  );
+}
+
+export default App;
